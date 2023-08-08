994ac4f2d43a29dde089d2f1328ccb6961ddfeb5
JakubC0I <JakubC.01@protonmail.com>
2023-08-08 16:02:37 +0200

    initial_commit

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,22 @@
+import {Task} from './Task'
+
+export const TASKS: Task[]=[
+    {
+        id: 1,
+        text: "Doctors Appointment",
+        day: "May 3rd at 2:30pm",
+        reminder: true
+    },
+    {
+        id: 2,
+        text: "Take the trash out",
+        day: "June 2nd at 8:00pm",
+        reminder: false
+    },
+    {
+        id: 3,
+        text: "Kiss your girlfriend",
+        day: "Everyday",
+        reminder: true
+    }
+]
